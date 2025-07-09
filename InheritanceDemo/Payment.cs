e22077bc84ebdb93380d4270092d927a59ed0dc8
Kandregula Durga Sai <kdurgas@apac.corpdir.net>
2025-07-09 09:06:36 +0530

    Inheritance

---
+++ InheritanceDemo/Payment.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    public class Payment
+    {
+
+        //private double _amount;
+
+        //public double Amount
+        //{
+        //    get
+        //    {
+        //        return _amount;
+        //    }
+        //    set
+        //    {
+        //        _amount = value;
+        //    }
+        //}
+
+        public double Amount { get; set; }
+        public string TransactionId;
+
+        public void EnterAmount()
+        {
+            Console.Write("Enter amount to pay: ");
+            Amount = Convert.ToDouble(Console.ReadLine());
+
+            TransactionId = Guid.NewGuid().ToString();
+        }
+
+        public virtual void Pay()
+        {
+            Console.WriteLine("Processing payment of ₹" + Amount);
+        }
+
+
+    }
+
+    class CreditCard : Payment
+    {
+        public string CardNumber;
+
+        public override void Pay()
+        {
+            Console.Write("Enter Credit Card Number: ");
+            CardNumber = Console.ReadLine();
+
+            Console.WriteLine($"Paid ₹{Amount} using Credit Card ending with {CardNumber.Substring(CardNumber.Length - 4)}.");
+            Console.WriteLine($"Transaction ID: {TransactionId}");
+        }
+
+    }
+}
---
+++ InheritanceDemo/Program.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Student s1 = new Student();
+            //Staff st1 = new Staff();
+            
+            B obj = new B();
+            obj.Method1();
+            obj.Method2();
+            obj.Method3();
+
+            A obj2 = new A();
+            obj2.Method2();
+            obj2.Method1();
+
+            //Single inheritance
+            //Multi level
+            //Hierarchical Inheritance
+            //Multiple Inheritance
+
+
+
+
+        }
+    }
+
+
+    class A
+    {
+        public void Method1()
+        {
+            Console.WriteLine("Method 1");
+        }
+
+        public void Method2()
+        {
+            Console.WriteLine("Method 2");
+        }
+    }
+
+    class B : A
+    {
+        //public void Method1()
+        //{
+        //    Console.WriteLine("Method 1");
+        //}
+
+        //public void Method2()
+        //{
+        //    Console.WriteLine("Method 2");
+        //}
+        public void Method3()
+        {
+            Console.WriteLine("Method 3");
+        }
+    }
+
+     class C : B
+    {
+
+    }
+
+    class D : B
+    {
+
+    }
+
+    class E : B
+    {
+
+    }
+
+    class F : D, E
+    {
+
+    }
+
+    //class F : E
+    //{
+
+    //}
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Phone { get; set; }
+    }
+
+
+
+    public class Student : Person
+    {
+
+        public string Class { get; set; }
+        public string Marks { get; set; }
+        public string Fees { get; set; }
+
+    }
+
+    public class  Staff : Person
+    {
+        public string Designation { get; set; }
+        public string Salary { get; set; }
+    }
+
+    public class TeachingStaff : Staff
+    {
+
+        public string Qualification { get; set; }
+        public string Subject { get; set; }
+
+    }
+
+    public class NonTeachingStaff : Staff
+    {
+        public string DeptName { get; set; }
+        public string ManagerId { get; set; }
+    }
+}
